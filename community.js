76efe05f0b93537cb6035e6ca748e975ef0f14f1
joelpang8 <67938850+joelpang8@users.noreply.github.com>
2026-09-22 22:21:27 -0500

    Delete community.js

--- community.js
+++
@@ -1,6 +0,0 @@
-const supremeCommunity = require("supreme-community-api");
-
-supremeCommunity()
-    .then((items) => console.log(items)) // [{name: 'Box Logo', price: '$50', image: 'http://'}, ...]
-    .catch((err) => console.log(err));
-
